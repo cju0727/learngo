6cf81820a75e8dd458fd90890fa9ce3e811f7255
cju0727 <chojw0727@gmail.com>
2022-03-20 23:57:20 +0900

    Upload something.go & vscode go extension practice

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cju0727/learngo/something"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello world")
+	something.SayHello()
 }
