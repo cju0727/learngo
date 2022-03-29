a39a8864bde259c8109ea7f3f9e6d3fe787a41ae
cju0727 <chojw0727@gmail.com>
2022-03-29 20:04:52 +0900

    Update main.go(functions part one)

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/cju0727/learngo/something"
 )
 
+// 자료형 지정1
+
+// func multiply(a int, b int) int {
+// 	return a * b
+// }
+
+// func main() {
+// 	fmt.Println((multiply(2, 2)))
+// }
+
+// 자료형 지정2
+
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+// func main() {
+// 	totalLenght, upperName := lenAndUpper("jaewoo")
+// 	fmt.Println(totalLenght, upperName)
+// }
+
+// 자료형 지정3
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
-	fmt.Println("Hello world")
-	something.SayHello()
-	var name string = "jaewoo"
-	name = "nico"
-	fmt.Println(name)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
