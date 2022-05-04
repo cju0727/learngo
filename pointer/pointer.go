c8111a4ef78615f5752a8eab56de03ae02ea8aba
cju0727 <chojw0727@gmail.com>
2022-05-04 18:42:21 +0900

    Upload pointer

---
+++ pointer/pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var a int = 500
+	// var p *int // int 포인터 변수 p 선언
+
+	// p = &a // a의 메모리 주소를 p에 대입(복사)
+
+	// fmt.Printf("p의 값 : %p\n", p)
+	// fmt.Printf("p가 가리키는 메모리의 값 : %d\n", *p)
+	// *p = 100
+	// fmt.Printf("a의 값 : %d\n", a)
+
+	var a int = 10
+	var b int = 20
+
+	var p1 *int = &a
+	var p2 *int = &a
+	var p3 *int = &b
+
+	fmt.Printf("p1 == p2 : %v\n", p1 == p2)
+	fmt.Printf("p2 == p3 : %v\n", p2 == p3)
+}
