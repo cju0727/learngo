a714a4c61e230fd232e4d17201596c0b22c12060
cju0727 <chojw0727@gmail.com>
2022-05-05 22:49:31 +0900

    Update pointer

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -2,7 +2,38 @@ package main
 
 import "fmt"
 
+// type Data struct {
+// 	value int
+// 	data  [200]int
+// }
+
+// func ChangeData(arg *Data) {
+// 	arg.value = 999
+// 	arg.data[100] = 999
+// }
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func NewUser(name string, age int) *User {
+	var u = User{name, age}
+	return &u
+}
+
 func main() {
+	userPointer := NewUser("AAA", 23)
+
+	fmt.Println(userPointer)
+
+	// var data Data
+
+	// ChangeData(&data)
+
+	// fmt.Printf("value = %d\n", data.value)
+	// fmt.Printf("data[100] = %d\n", data.data[100])
+
 	// var a int = 500
 	// var p *int // int 포인터 변수 p 선언
 
@@ -13,13 +44,15 @@ func main() {
 	// *p = 100
 	// fmt.Printf("a의 값 : %d\n", a)
 
-	var a int = 10
-	var b int = 20
+	/*
+		var a int = 10
+		var b int = 20
 
-	var p1 *int = &a
-	var p2 *int = &a
-	var p3 *int = &b
+		var p1 *int = &a
+		var p2 *int = &a
+		var p3 *int = &b
 
-	fmt.Printf("p1 == p2 : %v\n", p1 == p2)
-	fmt.Printf("p2 == p3 : %v\n", p2 == p3)
+		fmt.Printf("p1 == p2 : %v\n", p1 == p2)
+		fmt.Printf("p2 == p3 : %v\n", p2 == p3)
+	*/
 }
