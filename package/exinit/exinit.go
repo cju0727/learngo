d1023bd3fe57c6df3019c58b230c113fe88c86d9
cju0727 <chojw0727@gmail.com>
2022-05-17 23:38:20 +0900

    Upload package

---
+++ package/exinit/exinit.go
@@ -0,0 +1,24 @@
+package exinit
+
+import "fmt"
+
+var (
+	a = b + f()
+	b = c
+	c = f()
+	d = 3
+)
+
+func init() {
+	fmt.Printf("init function a:%d b:%d c:%d d:%d\n", a, b, c, d)
+}
+
+func f() int {
+	d++
+	fmt.Println("f() d:", d)
+	return d
+}
+
+func PrintD() {
+	fmt.Println("d:", d)
+}
---
+++ package/package.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cju0727/learngo/package/exinit"
+)
+
+func main() {
+	fmt.Println("main function")
+	exinit.PrintD()
+}
