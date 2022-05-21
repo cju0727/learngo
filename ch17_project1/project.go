2fc986a1ced8ebb929d2956b5bc3a467dc12bd7a
cju0727 <chojw0727@gmail.com>
2022-05-21 21:57:35 +0900

    Update ch17_project1(숫자값 입력받기)

--- ch17_project1/project.go
+++ ch17_project1/project.go
@@ -1,5 +1,36 @@
 package main
 
+// 숫자값 입력받기
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var stdin = bufio.NewReader(os.Stdin)
+
+func InputIntValue() (int, error) {
+	var n int
+	_, err := fmt.Scanln(&n)
+	if err != nil {
+		stdin.ReadString('\n')
+	}
+	return n, err
+}
+
+func main() {
+	for {
+		fmt.Printf("숫자값을 입력하세요:")
+		n, err := InputIntValue()
+		if err != nil {
+			fmt.Println("숫자만 입력하세요.")
+		} else {
+			fmt.Println("입력하신 숫자는 ", n, "입니다.")
+		}
+	}
+}
+
+/* 랜덤한 숫자 생성하기
 import (
 	"fmt"
 	"math/rand"
@@ -12,3 +43,4 @@ func main() {
 	n := rand.Intn(100)
 	fmt.Println(n)
 }
+*/
