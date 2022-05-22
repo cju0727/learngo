1b6c4bb41782f9ad8622527e5ba6aa1f47dd60d8
cju0727 <chojw0727@gmail.com>
2022-05-22 16:18:46 +0900

    Update ch17_project1(숫자 맞추기 완성)

--- ch17_project1/project.go
+++ ch17_project1/project.go
@@ -1,6 +1,51 @@
 package main
 
-// 숫자값 입력받기
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+// 숫자 맞추기 완성
+
+var stdin = bufio.NewReader(os.Stdin)
+
+func InputIntValue() (int, error) {
+	var n int
+	_, err := fmt.Scanln(&n)
+	if err != nil {
+		stdin.ReadString('\n')
+	}
+	return n, err
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	r := rand.Intn(100)
+	cnt := 1
+	for {
+		fmt.Printf("숫자값을 입력하세요.")
+		n, err := InputIntValue()
+		if err != nil {
+			fmt.Println("숫자만 입력하세요.")
+		} else {
+			if n > r {
+				fmt.Println("입력하신 숫자가 더 큽니다.")
+			} else if n < r {
+				fmt.Println("입력하신 숫자가 더 작습니다.")
+			} else {
+				fmt.Println("숫자를 맞췄습니다. 축하합니다. 시도한 횟수:", cnt)
+				break
+			}
+			cnt += 1
+		}
+	}
+}
+
+/* 숫자값 입력받기
 import (
 	"bufio"
 	"fmt"
@@ -29,6 +74,7 @@ func main() {
 		}
 	}
 }
+*/
 
 /* 랜덤한 숫자 생성하기
 import (
