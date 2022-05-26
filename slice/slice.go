376366c0ab837b504c601e468799f6b543ee94ee
cju0727 <chojw0727@gmail.com>
2022-05-26 22:05:59 +0900

    Update slice(ch18_2)

--- slice/slice.go
+++ slice/slice.go
@@ -3,14 +3,74 @@ package main
 import "fmt"
 
 func main() {
-	var slice []int
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
 
-	for i := 1; i <= 10; i++ {
-		slice = append(slice, i)
-	}
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
 
-	slice = append(slice, 11, 12, 13, 14, 15)
-	fmt.Println(slice)
+	slice1[1] = 100
+
+	fmt.Println("After change second element")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice1 = append(slice1, 500)
+
+	fmt.Println("After append 500")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	/*slice append 예시 1
+	slice1 := make([]int, 3, 5)
+	slice2 := append(slice1, 4, 5)
+
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice1[1] = 100
+
+	fmt.Println("After change second element")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+
+	slice1 = append(slice1, 500)
+
+	fmt.Println("After append 500")
+	fmt.Println("slice1:", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2:", slice2, len(slice2), cap(slice2))
+	*/
+
+	/*array & slice
+	  func changeArray(array2 [5]int) {
+	  	array2[2] = 200
+	  }
+
+	  func changeSlice(slice2 []int) {
+	  	slice2[2] = 200
+	  }
+
+	  func main() {
+	  	array := [5]int{1, 2, 3, 4, 5}
+	  	slice := []int{1, 2, 3, 4, 5}
+
+	  	changeArray(array)
+	  	changeSlice(slice)
+
+	  	fmt.Println(array)
+	  	fmt.Println(slice)
+	*/
+
+	/*
+		var slice []int
+
+		for i := 1; i <= 10; i++ {
+			slice = append(slice, i)
+		}
+
+		slice = append(slice, 11, 12, 13, 14, 15)
+		fmt.Println(slice)
+	*/
 
 	/* append 1
 	var slice = []int{1, 2, 3}
